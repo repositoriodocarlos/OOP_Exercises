27da182c04c8b91c4866a36465d102e926014d7f
cteixeira <carlos.teixeira@capgemini.com>
2021-03-14 12:47:48 -0300

    exercicio 07

---
+++ Exercise_07/JogoDinossauro/src/view/Teste.java
@@ -0,0 +1,49 @@
+package view;
+import java.util.Scanner;
+import controller.Dinossauro;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Dinossauro Skeep =  new Dinossauro("Skeep");
+		
+		boolean loop = true;
+		 while(loop == true){
+			 Scanner scan = new Scanner(System.in);
+			 System.out.println(Skeep);
+			 System.out.println("Escolha uma ação para o " + Skeep.nome + ": \n" 
+					 			 + "(P)ular \n"
+					 			 + "Co(R)rer \n"
+					 			 + "(C)omer \n"
+					 			 + "(B)rincar\n"
+					 			 + "Tomar (S)ol \n"
+					 			 + "Ficar na S(o)mbra");
+			 String textoMaiusculo = scan.nextLine().toUpperCase();
+			 char letra = textoMaiusculo.charAt(0);
+			 switch(letra){
+			 	case 'P':
+			 		Skeep.pular();
+			 		break;
+			 	case 'R':
+			 		Skeep.correr();
+			 		break;
+			 	case 'C':
+			 		Skeep.comer();
+			 		break;
+			 	case 'B':
+			 		Skeep.brincar();
+			 		break;
+			 	case 'S':
+			 		Skeep.tomarSol();
+			 		break;
+			 	case 'O':
+			 		Skeep.ficarNaSombra();			 		
+			 		break;			 				
+			 }
+			 
+		 }
+
+
+	}
+
+}
